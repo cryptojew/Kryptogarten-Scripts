24d71cfa6c11772bf3a3940db6c4c81ef1a7d2b8
cryptojew <zoharstartup@gmail.com>
2016-04-20 19:31:15 -0400

    Update keygen.py
    Added private / public keys functions.

--- py/keygen.py
+++ py/keygen.py
@@ -18,3 +18,30 @@ print "Private key(dec) is: ", decoded_private_key
 #Convert private key to WIF format
 wif_encoded_private_key = bitcoin.encode_privkey(decoded_private_key, 'wif')
 print "Private key(wif) is: ", private_key
+
+#Add suffix 01 to indicate a compressed private key
+compressed_private_key = private_key + '01'
+print "Private key compressed (hex) is: ", compressed_private_key
+
+#Generate a WIF format from the compressed private key (WIF-compressed) 
+wif_compressed_private_key = bitcoin.encode_privkey(bitcoin_decode_privkey(compressed_private_key, 'hex'), 'wif')
+print "Private key (WIF-compressed) is: ", wif_compressed_private_key 
+
+#Multiply the EC generator point G with the private key to get a public key point 
+public_key = bitcoin.fast_multiply(bitcoin.G, decoded_private_key)
+print "Public key (x,y) coordinates is: ", public_key
+
+#Encode as hex, prefix 04 
+hex_encoded_public_key = bitcoin.encode_pubkey(public_key, 'hex')
+print "Public key (hex) is: ", hex_encoded_public_key 
+
+#Compress public key, adjust prefix depending on whether y is even or odd 
+(public_key_x, public_key_y) = public_key
+if(public_key_y % 2) == 0: 
+  compressed_prefix = '02'
+else:
+  compressed_prefix = '03'
+hex_compressed_public_key = compressed_prefix + bitcoin.encode(public_key_x, 16)
+print "Compressed Public key (hex) is: ", hex_compressed_public_key
+
+
